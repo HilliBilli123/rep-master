d20381d277d3848c279f8bf4ddffe3cc707dc18f
bugsBagi <bakhtyar@tamur.kz>
2022-03-28 19:45:29 +0600

    producrs add and edit

--- inc/productEdit.php
+++ inc/productEdit.php
@@ -1,14 +1,24 @@
 <?php
     session_start();
+    require_once 'connect.php';
     /*if(!$_SESSION['user']){
         header('Location: ../index.php');
     }*/
     $id = $_POST["id"];
     $name = $_POST['name'];
     $price = $_POST['price'];
-    include "../inc/connect.php";
-    //$connect = mysqli_connect('localhost', 'root' , 'root' , 'bibala');
-    mysqli_query($connect, "UPDATE `products` SET `name` = '$name', `price` = '$price' WHERE `id` = '$id' ");
+    $harackter = $_POST['harackter'];
+    $category = $_POST['nameCategory'];
+    $path = 'admin/upload/'.time().$_FILES['file']['name'];
+    if($_FILES["file"]["name"]){
+        if(!move_uploaded_file($_FILES['file']['tmp_name'], '../'. $path)){
+            $_SESSION['message'] = 'Ошибка при загрузке фото';
+            header('Location: ../pages/manufacturesMain.php');
+        }
+        mysqli_query($connect, "UPDATE `products` SET `name` = '$name', `harackter` = '$harackter', `type` = '$category', `price` = '$price' WHERE `id` = '$id' ");
+    }  else{
+        $_SESSION['message'] = 'Загрузите фото';
+    }
     //ini_set('date.timezone', 'Asia/Almaty');
     header('Location: ../pages/productMain.php');
 ?>
