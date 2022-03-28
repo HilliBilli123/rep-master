858cb04e5390b83fea07b5fde9caa4fa7a976e9c
Temirhan <qwe.mister.qwe@bk.ru>
2022-03-28 14:40:37 +0600

    userMain

--- pages/userMain.php
+++ pages/userMain.php
@@ -21,7 +21,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/admin.css">
     <title>User Panel</title>
 </head>
 <body>
@@ -29,51 +29,115 @@
     <!-- <form action="" method = "post" enctype = "multipart/form-data"> -->
     
     <!-- </form> -->
-    <a href="userAdd.php">Добавить</a>
-    <table style="border:1px solid #000;">
-            <tr>
-                <td style="border:1px solid #000;">№</td>
-                <td style="border:1px solid #000;">login</td>
-                <td style="border:1px solid #000;">password</td>
-                <td style="border:1px solid #000;">city</td>
-                <td style="border:1px solid #000;">lastName</td>
-                <td style="border:1px solid #000;">firstName</td>
-                <td style="border:1px solid #000;">middleName</td>
-                <td style="border:1px solid #000;">email</td>
-                <td style="border:1px solid #000;">phone</td>
-                <td style="border:1px solid #000;">редактировать</td>
-                <td style="border:1px solid #000;">удалить</td>
-            </tr>
+    <div class="user flex__content">
+        <div class="menu__admin__pages">
+            <div class="menu__admin__pages__contain">
+                <div class="menu__admin__pages__logo">
+                    <img src="../response/image/logo.png" alt="">
+                </div>
+                <div class="menu__admin__pages__list">
+                    <a href="admin.php" class="manufact__link">На главную</a>
+                    <a href="" class="manufact__link">Товаровы</a>
+                    <a href="" class="manufact__link">Производители</a>
+                    <a href="" class="manufact__link">Категории</a>
+                    <a href="" class="manufact__link">Пользоваетели</a>
+                    <!-- <a href="" class="manufact__link"></a>
+                    <a href="" class="manufact__link"></a> -->
+                </div>
+            </div>
+        </div>
+    <!-- <a href="userAdd.php">Добавить</a> -->
+    <div class="table">
+            <div class="buttons__add">
+                <a href="" class="button__add">Добавить</a>
+                <div class="popap__window"> 
+                    <div class="popap__window__conten">
+                        <form action="../inc/userAdd.php" enctype="multipart/form-data" method="post" class="popap__form">    
+                            <div class="popap__title">Редактирование</div>
+                            <div class="popap__out">X</div>
+                            <div class="popap__all">
+                                <div class="popap__text">
+                                    <label for="" class="popap__lable" >Описание на русском</label>
+                                    <input name="text" type="text" class="popap__lable">
+                                </div>
+                                <div class="popap__text">
+                                    <label for="" class="popap__lable" >Описание на казахском</label>
+                                    <input type="text" name="textKz" class="popap__lable">
+                                </div>
+                                <input type="file" name="file" id="" accept="image/jpeg,image/png">
+                                <br>
+                                <button type="submit">Сохронить</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="table__contain">
+            <div class="table__content flex__content table__header">
+                <div class="table__title__number">№</div>
+                <div class="table__title">login</div>
+                <div class="table__title">password</div>
+                <div class="table__title">city</div>
+                <div class="table__title">lastName</div>
+                <div class="table__title">firstName</div>
+                <div class="table__title">middleName</div>
+                <div class="table__title">email</div>
+                <div class="table__title">phone</div>
+                <div class="table__title">редактировать</div>
+                <div class="table__title">удалить</div>
+            </div>
         <?php
             $count = 0;
             foreach($result as $product)
             {
                 $count++;
         ?>
-            <tr>
-                <td><?php echo $count?></td>
-                <td><?php echo $product['login']?></td>
-                <td><?php echo $product['password']?></td>
-                <td><?php echo $product['cityName']?></td>
-                <td><?php echo $product['lastName']?></td>
-                <td><?php echo $product['firstName']?></td>
-                <td><?php echo $product['middleName']?></td>
-                <td><?php echo $product['email']?></td>
-                <td><?php echo $product['phone']?></td>
-                <td>
-                    <form action="userEdit.php">
-                        <a href="userEdit.php?id=<?php echo $product['id']; ?>">редактировать</a>
-                    </form>
-                </td>
-                <td>
+            <div class="table__content flex__content">
+                <div class="table__title__number"><?php echo $count?></div>
+                <div class="table__title"><?php echo $product['login']?></div>
+                <div class="table__title"><?php echo $product['password']?></div>
+                <div class="table__title"><?php echo $product['cityName']?></div>
+                <div class="table__title"><?php echo $product['lastName']?></div>
+                <div class="table__title"><?php echo $product['firstName']?></div>
+                <div class="table__title"><?php echo $product['middleName']?></div>
+                <div class="table__title"><?php echo $product['email']?></div>
+                <div class="table__title"><?php echo $product['phone']?></div>
+                <div class="table__title">
+                    <a class="icon-edit" href=""></a>
+                    <div class="popap__window"> 
+                        <div class="popap__window__conten">
+                            <form action="../inc/userEdit.php" enctype="multipart/form-data" method="post" class="popap__form">    
+                                <div class="popap__title">Редактирование</div>
+                                <div class="popap__out">X</div>
+                                <div class="popap__all">
+                                    <div class="popap__text">
+                                        <label for="" class="popap__lable" >Описание на русском</label>
+                                        <input name="text" type="text" class="popap__lable" value="<?php echo $product['text']?>">
+                                    </div>
+                                    <div class="popap__text">
+                                        <label for="" class="popap__lable" >Описание на казахском</label>
+                                        <input type="text" name="textKz" class="popap__lable" value="<?php echo $product['textKz'] ?>">
+                                    </div>
+                                    <input type="file" name="file" id="" accept="image/jpeg,image/png">
+                                    <input type="text" name="id" style="display: none;" value="<?php echo $product['id']?>"> 
+                                    <br>
+                                    <button type="submit">Сохронить</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="table__title">
                 <form action="../inc/userDelete.php">
-                    <a href="../inc/userDelete.php?id=<?php echo $product['id']; ?>">удалить</a>
+                    <a class="icon-bin" href="../inc/userDelete.php?id=<?php echo $product['id']; ?>"></a>
                 </form>
-                </td>
-            </tr>
+                </div>
+            </div>
         <?php
             }
         ?>
-    </table>
+            </div>
+    </div>
+    </div>
 </body>
 </html>
