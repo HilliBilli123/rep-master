d05d738722d6f9db693213b923f32c9a4f003760
Temirhan <qwe.mister.qwe@bk.ru>
2022-03-29 19:13:37 +0600

    header

--- pages/products.php
+++ pages/products.php
@@ -1,13 +1,13 @@
 <?php
-    session_start();
-    /*if(!$_SESSION['user']){
+session_start();
+/*if(!$_SESSION['user']){
         header('Location: ../index.php');
     }*/
-    include "../inc/connect.php";
-    //$connect = mysqli_connect('localhost', 'root' , 'root' , 'bibala');
-    $result = mysqli_query($connect,"SELECT products.*, category.nameCategory, category.nameCategoryKz, category.price FROM `products` join category on category.id = products.type");
-    //print_r($result);
-    //ini_set('date.timezone', 'Asia/Almaty');
+include "../inc/connect.php";
+//$connect = mysqli_connect('localhost', 'root' , 'root' , 'bibala');
+$result = mysqli_query($connect, "SELECT products.*, category.nameCategory, category.nameCategoryKz, category.price FROM `products` join category on category.id = products.type");
+//print_r($result);
+//ini_set('date.timezone', 'Asia/Almaty');
 ?>
 
 <!DOCTYPE html>
@@ -23,49 +23,50 @@
 </head>
 
 <body>
-    <header class="header__main header_mainProduct">
+	<header class="header__main">
 		<div class="header__body _contein">
 			<ul class="header__body__menu">
 				<li class="menu__logo"><img src="../response/image/logo.png" alt="logo"></li>
-				<li class="menu__link"><a href="../index.html">Главная</a></li>
-				<li class="menu__link"><a href="products.php">Товары</a></li>
-				<li class="menu__link"><a href="">Услуги</a></li>
-				<li class="menu__link"><a href="">Корзина</a></li>
+				<li class="menu__link"><a href="../index.php">Главная</a></li>
+				<li class="menu__link"><a href="../pages/products.php">Товары</a></li>
+				<li class="menu__link"><a href="../pages/basket.php">Корзина</a></li>
 				<li class="menu__link"><a href="">Контакты</a></li>
-				<li>
+				<!-- <li>
 					<ul class="menu__lang">
-						<img src="../response/image/flagKz.png" alt="">
-						<img src="../response/image/flagRu.png" alt="">
+						<img src="response/image/flagKz.png" alt="">
+						<img src="response/image/flagRu.png" alt="">
 					</ul>
-				</li>
+				</li> -->
+				<li class="menu__link menu__auth"><a href="/pages/singin.php">
+						<img src="../response/image/userIcon.png" alt="">
+					</a></li>
 			</ul>
 		</div>
 	</header>
-    <section class="page__products products">
-        <div class="products-container _contein">
+	<section class="page__products products">
+		<div class="products-container _contein">
 			<h2 class="products__title _title"></h2>
 			<div class="products__items">
 				<?php
-				foreach($result as $product)
-					{
+				foreach ($result as $product) {
 				?>
 					<div class="products__item item__product">
 						<div class="item__product__image _ibg">
-							<img src="../<?php echo $product['pathImage']?>" alt="">
+							<img src="../<?php echo $product['pathImage'] ?>" alt="">
 						</div>
-						<div class="nameAndType"> 
-							<p><?php echo $product['name']?></p>
-							<p><?php echo $product['nameCategory']?></p>
+						<div class="nameAndType">
+							<p><?php echo $product['name'] ?></p>
+							<p><?php echo $product['nameCategory'] ?></p>
 						</div>
 						<a href="productCalculate.php?id=<?php echo $product['id']; ?>">Подробнее</a>
 					</div>
 				<?php
-					}
+				}
 				?>
-			</div>	
-		</div>	
-    </section>
-    <footer class="footer">
+			</div>
+		</div>
+	</section>
+	<footer class="footer">
 		<div class="footer__cpntainer _contein">
 			<div class="footer__body">
 
@@ -93,4 +94,5 @@
 	<script src="js/swiper-bundle.min.js"></script>
 	<script src="js/script.js"></script>
 </body>
+
 </html>
