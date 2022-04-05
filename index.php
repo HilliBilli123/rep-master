574ee38345296067007e3360d3e41bc87cae4307
Temirhan <qwe.mister.qwe@bk.ru>
2022-04-05 19:33:41 +0600

    2131231

--- index.php
+++ index.php
@@ -39,95 +39,97 @@
 		</div>
 	</header>
 	<section class="main">
-		<div class="slider _contein">
-			<div class="image-slider swiper">
-				<div class="image-slider__wrapper swiper-wrapper">
-					<div class="image-slider__slide swiper-slide">
-						<div class="image-slider_content">
-							<div class="image-slider__image _ibg">
-								<img src="response/slider/window.png" alt="no Image">
-							</div>
-							<div class="image-slider__contain slider-contain">
-								<div class="slider-contain__title">
-									Пластиковые окна
+		<div class="slider__color">
+			<div class="slider _contein">
+				<div class="image-slider swiper">
+					<div class="image-slider__wrapper swiper-wrapper">
+						<div class="image-slider__slide swiper-slide">
+							<div class="image-slider_content">
+								<div class="image-slider__image _ibg">
+									<img src="response/slider/window.png" alt="no Image">
 								</div>
-								<div class="slider-contain__text">
-									Материалы высшего качества
-								</div>
-								<div class="slider-contain__text">
-									Выбирайте только лучшее
-								</div>
-								<div class="slider-contain__buttons">
-									<a href="#about" class="slider-contain__button">Узнать</a>
+								<div class="image-slider__contain slider-contain">
+									<div class="slider-contain__title">
+										Пластиковые окна
+									</div>
+									<div class="slider-contain__text">
+										Материалы высшего качества
+									</div>
+									<div class="slider-contain__text">
+										Выбирайте только лучшее
+									</div>
+									<div class="slider-contain__buttons">
+										<a href="#about" class="slider-contain__button">Узнать</a>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<div class="image-slider__slide swiper-slide">
-						<div class="image-slider_content">
+						<div class="image-slider__slide swiper-slide">
+							<div class="image-slider_content">
+								<div class="image-slider__image _ibg">
+									<img src="response/slider/door.png" alt="no Image">
+								</div>
+								<div class="image-slider__contain slider-contain">
+									<div class="slider-contain__title">
+										Пластиковые двери
+									</div>
+									<div class="slider-contain__text fontSize36">
+										Воплощайте идеи
+									</div>
+									<div class="slider-contain__text fontSize36">
+										Надежные для <br> профессиональных работ
+									</div>
+									<div class="slider-contain__buttons">
+										<a href="#specification" class="slider-contain__button">Узнать</a>
+									</div>
+								</div>
+							</div>	
+						</div>
+						<!-- <div class="image-slider__slide swiper-slide">
 							<div class="image-slider__image _ibg">
-								<img src="response/slider/door.png" alt="no Image">
+								<img src="response/slider/slider3.jpg" alt="no Image">
 							</div>
 							<div class="image-slider__contain slider-contain">
-								<div class="slider-contain__title">
-									Пластиковые двери
-								</div>
 								<div class="slider-contain__text fontSize36">
 									Воплощайте идеи
+								</div>
+								<div class="slider-contain__title fontSize64">
+									Европейское качество
 								</div>
 								<div class="slider-contain__text fontSize36">
 									Надежные для <br> профессиональных работ
 								</div>
 								<div class="slider-contain__buttons">
-									<a href="#specification" class="slider-contain__button">Узнать</a>
+									<a href="" class="slider-contain__button">Узнать</a>
 								</div>
 							</div>
-						</div>	
+						</div>
+						<div class="image-slider__slide swiper-slide">
+							<div class="image-slider__image _ibg">
+								<img src="response/slider/slider4.jpg" alt="no Image">
+							</div>
+							<div class="image-slider__contain slider-contain">
+								<div class="slider-contain__text fontSize36">
+									Воплощайте идеи
+								</div>
+								<div class="slider-contain__title fontSize64">
+									Европейское качество
+								</div>
+								<div class="slider-contain__text fontSize36">
+									Надежные для <br> профессиональных работ
+								</div>
+								<div class="slider-contain__buttons">
+									<a href="" class="slider-contain__button">Узнать</a>
+								</div>
+							</div>
+						</div> -->
 					</div>
-					<!-- <div class="image-slider__slide swiper-slide">
-						<div class="image-slider__image _ibg">
-							<img src="response/slider/slider3.jpg" alt="no Image">
-						</div>
-						<div class="image-slider__contain slider-contain">
-							<div class="slider-contain__text fontSize36">
-								Воплощайте идеи
-							</div>
-							<div class="slider-contain__title fontSize64">
-								Европейское качество
-							</div>
-							<div class="slider-contain__text fontSize36">
-								Надежные для <br> профессиональных работ
-							</div>
-							<div class="slider-contain__buttons">
-								<a href="" class="slider-contain__button">Узнать</a>
-							</div>
-						</div>
-					</div>
-					<div class="image-slider__slide swiper-slide">
-						<div class="image-slider__image _ibg">
-							<img src="response/slider/slider4.jpg" alt="no Image">
-						</div>
-						<div class="image-slider__contain slider-contain">
-							<div class="slider-contain__text fontSize36">
-								Воплощайте идеи
-							</div>
-							<div class="slider-contain__title fontSize64">
-								Европейское качество
-							</div>
-							<div class="slider-contain__text fontSize36">
-								Надежные для <br> профессиональных работ
-							</div>
-							<div class="slider-contain__buttons">
-								<a href="" class="slider-contain__button">Узнать</a>
-							</div>
-						</div>
-					</div> -->
+					<!-- Стрелки -->
+					<div class="swiper-button-prev"></div>
+					<div class="swiper-button-next"></div>
+					<!-- Пагинация -->
+					<!-- <div class="swiper-pagination"></div> -->
 				</div>
-				<!-- Стрелки -->
-				<div class="swiper-button-prev"></div>
-				<div class="swiper-button-next"></div>
-				<!-- Пагинация -->
-				<!-- <div class="swiper-pagination"></div> -->
 			</div>
 		</div>
 		<div id="about" class="about">
@@ -184,7 +186,19 @@
 				</div>
 			</div>
 		</div>
-		.
+		<div id="about3" class="about">
+			<div class="main__about__photo _contein">
+				<div class="personna__photo">
+					<img src="response/image/pngwing.com.png" alt="">
+				</div>
+					<!-- <img src="response/image/Знак-качества_Хай.svg" alt=""> -->
+				<div class="personna__text">
+					<div class="text__contein__title">Высочайшее качество</div>
+					<div class="text__contein__text">В производстве пластиковых окон наша компания использует
+						германский профиль производства </div>
+				</div>
+			</div>
+		</div>
 		<div id="specification" class="specification">
 			<div class="swiper specification__slider _contein specification__slider__contein">
 				<!-- Additional required wrapper -->
